7b66c33a1560e4741eaf93a424263714939d9c93
Astrial <mlsgamer09@gmail.com>
2025-09-10 13:09:24 -0500

    Update storyMenu.hx

---
+++ data/states/storyMenu.hx
@@ -0,0 +1,20 @@
+static var storyMenuIndex = 0;
+
+var weekList:Array<String> =
+[
+    "prelude",
+    "week1",
+    "week2",
+    "week3",
+    "week4",
+    "week5",
+    "week6",
+    "epilogue"
+];
+
+var diffList:Array<String> = 
+[
+    "easy",
+    "normal", 
+    "hard"
+];
