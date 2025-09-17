7ecd48dc90109f2052ddf7d98517d61e713dfafd
Astrial <mlsgamer09@gmail.com>
2025-09-17 10:30:40 -0500

    dialogue revamp: no cigar

--- songs/no cigar/dialogue.hx
+++ songs/no cigar/dialogue.hx
@@ -1,37 +1,66 @@
-function next(event)
-{
-	if (!event.playFirst) trace("-");
-}
+import haxe.xml.Access;
+
+// Var things
+var songThing = "no-cigar";
+
+var curVoiceline;
+
+var dialogueFilePath = "dio/sounds/" + songThing + "/";
+
+var voiceNumShit = -1;
+
+var bgSpriteTraceShit;
+
+var dioList:Array<String> = 
+[
+    "1",
+    "2", 
+	"3", 
+	"4", 
+	"5", 
+	"6",
+	"none", 
+	"7", 
+	"8", 
+	"9", 
+	"10"
+];
+
+//BG Sprite
 
 var bgSprite1:FunkinSprite = new FunkinSprite(0,0);
-
 insert(members.indexOf = 1, bgSprite1);
 add(bgSprite1);
+
+// Functions
 
 function bgEdit(sprite)
 {	
 	
 	bgSprite1.loadGraphic(Paths.image("dialogue/backgrounds/"+sprite));
+	bgSpriteTraceShit = sprite;
+	trace("Background sprite set to: " + '"' +bgSpriteTraceShit + '"');
 	
 }
 
 function postNext(event)
 {
+	handleVoices();
 	trace(curLine.char + " says: " + curLine.text);
-	if (curLine.char == "pico")
+	if (voiceNumShit == 0)
 	{
 		bgEdit("alleyway");
-		trace("sprite made");
+		
 	}
-	if (curLine.char == "")
-	{
-		bgEdit("no-cigar_GRIMACEBLAST");
-		dialogueBox.visible = false;
-		trace("sprite made");
-	}
-	else
-	{
-		dialogueBox.visible = true;
-	}
+	
+}
 
+function handleVoices()
+{
+	voiceNumShit++;
+	if (curVoiceline != null) curVoiceline.stop();
+	if (voiceNumShit != 7)
+	{
+		curVoiceline = FlxG.sound.play(Paths.sound(dialogueFilePath+dioList[voiceNumShit]));
+	}
 }
