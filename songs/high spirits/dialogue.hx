3388a734ca3fa38686e7e38755194a9fca9aadf9
Astrial <mlsgamer09@gmail.com>
2025-09-17 11:15:47 -0500

    dialogue revamp: high spirits

--- songs/high spirits/dialogue.hx
+++ songs/high spirits/dialogue.hx
@@ -1,22 +1,69 @@
-function next(event)
-{
-	if (!event.playFirst) trace("-");
-}
+import haxe.xml.Access;
+
+// Var things
+var songThing = "high-spirits";
+
+var curVoiceline;
+
+var dialogueFilePath = "dio/sounds/" + songThing + "/";
+
+var voiceNumShit = -1;
+
+var bgSpriteTraceShit;
+
+var dioList:Array<String> = 
+[
+    "1",
+    "2",
+	"3", 
+	"4", 
+	"5", 
+	"6",
+	"7", 
+	"8", 
+	"9", 
+	"10", 
+	"11", 
+	"12", 
+	"13", 
+	"14"
+];
+
+//BG Sprite
 
 var bgSprite1:FunkinSprite = new FunkinSprite(0,0);
-
 insert(members.indexOf = 1, bgSprite1);
 add(bgSprite1);
+
+// Functions
 
 function bgEdit(sprite)
 {	
 	
 	bgSprite1.loadGraphic(Paths.image("dialogue/backgrounds/"+sprite));
+	bgSpriteTraceShit = sprite;
+	trace("Background sprite set to: " + '"' +bgSpriteTraceShit + '"');
 	
 }
 
 function postNext(event)
 {
+	handleVoices();
 	trace(curLine.char + " says: " + curLine.text);
-	bgEdit("spooky-room");
+	if (voiceNumShit == 0)
+	{
+		bgEdit("spooky-room");
+		
+	}
+	
+}
+
+function handleVoices()
+{
+	voiceNumShit++;
+	if (curVoiceline != null) curVoiceline.stop();
+	
+	
+	curVoiceline = FlxG.sound.play(Paths.sound(dialogueFilePath+dioList[voiceNumShit]));
+	
 }
