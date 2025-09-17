78ac252df545f3406745ca5163aa6f9114e9a5d4
Astrial <mlsgamer09@gmail.com>
2025-09-17 11:37:11 -0500

    dialogue revamp: repressed end

--- songs/repressed/end-dialogue.hx
+++ songs/repressed/end-dialogue.hx
@@ -1,58 +1,93 @@
-function next(event)
-{
-	if (!event.playFirst) trace("-");
-}
+import haxe.xml.Access;
+
+// Var things
+var songThing = "repressed";
+
+var curVoiceline;
+
+var dialogueFilePath = "dio/sounds/" + songThing + "/";
+
+var voiceNumShit = -1;
+
+var bgSpriteTraceShit;
+
+var dioList:Array<String> = 
+[
+    "1end",
+    "2end",
+	"3end", 
+	"4end", 
+	"5end", 
+	"6end",
+	"7end", 
+	"8end", 
+	"9end", 
+	"10end", 
+	"11end", 
+	"noneend", 
+	"12end", 
+	"noneend", 
+	"beep", 
+	"13end", 
+	"14end"
+];
+
+//BG Sprite
 
 var bgSprite1:FunkinSprite = new FunkinSprite(0,0);
-
 insert(members.indexOf = 1, bgSprite1);
 add(bgSprite1);
+
+// Functions
 
 function bgEdit(sprite)
 {	
 	
 	bgSprite1.loadGraphic(Paths.image("dialogue/backgrounds/"+sprite));
-	
-}
-
-function bgCreateSpookyRoomDark()
-{	
-	
-	
-	bgSprite1.loadGraphic(Paths.image("dialogue/backgrounds/spooky-room-dark"));
-
-	
-}
-
-function bgCreateSpookyRoom()
-{	
-	
-	
-	bgSprite1.loadGraphic(Paths.image("dialogue/backgrounds/spooky-room"));
-
+	bgSpriteTraceShit = sprite;
+	trace("Background sprite set to: " + '"' +bgSpriteTraceShit + '"');
 	
 }
 
 function postNext(event)
 {
+	handleVoices();
 	trace(curLine.char + " says: " + curLine.text);
-	if (curLine.char == "pico-left")
+	if (voiceNumShit == 0)
 	{
 		bgEdit("spooky-room");
-		trace("sprite made: Spooky Room");
+		
 	}
-	if (curLine.text == "(FLASH)")
+	if (voiceNumShit == 14)
 	{
 		bgEdit("lowtiergod");
 		bgSprite1.scale.set(0.5,0.5);
 		bgSprite1.updateHitbox();
-		trace("sprite made: Low Teir God");
+		
 	}
-	if (curLine.text == "Beep!")
+	if (voiceNumShit == 15)
 	{
 		bgEdit("spooky-room");
 		bgSprite1.scale.set(1,1);
 		bgSprite1.updateHitbox();
-		trace("sprite made: Spooky Room");
 	}
+	
+}
+
+function handleVoices()
+{
+	voiceNumShit++;
+	
+	if (curVoiceline != null) curVoiceline.stop();
+	
+	if (voiceNumShit != 12 && voiceNumShit != 14)
+	{
+		curVoiceline = FlxG.sound.play(Paths.sound(dialogueFilePath+dioList[voiceNumShit]));
+	}
+	if (voiceNumShit == 14)
+	{
+		curVoiceline = FlxG.sound.play(Paths.sound("dio/sounds/lowtiergod"));
+	}
+	
+	
 }
