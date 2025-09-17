9d26b79918f51716adec0100ba0db366b562eda9
Astrial <mlsgamer09@gmail.com>
2025-09-17 11:23:48 -0500

    dialogue revamp: repressed

--- songs/repressed/dialogue.hx
+++ songs/repressed/dialogue.hx
@@ -1,54 +1,74 @@
-function next(event)
-{
-	if (!event.playFirst) trace("-");
-}
+import haxe.xml.Access;
+
+// Var things
+var songThing = "repressed";
+
+var curVoiceline;
+
+var dialogueFilePath = "dio/sounds/" + songThing + "/";
+
+var voiceNumShit = -1;
+
+var bgSpriteTraceShit;
+
+var dioList:Array<String> = 
+[
+    "1",
+    "2",
+	"3", 
+	"4", 
+	"5", 
+	"6",
+	"7", 
+	"8", 
+	"monster1", 
+	"10", 
+	"11", 
+	"monster5", 
+	"13", 
+	"monster2", 
+	"15", 
+	"16", 
+	"17", 
+	"18", 
+	"monster3"
+];
+
+//BG Sprite
 
 var bgSprite1:FunkinSprite = new FunkinSprite(0,0);
-
 insert(members.indexOf = 1, bgSprite1);
 add(bgSprite1);
+
+// Functions
 
 function bgEdit(sprite)
 {	
 	
 	bgSprite1.loadGraphic(Paths.image("dialogue/backgrounds/"+sprite));
-	
-}
-
-function bgCreateSpookyRoomDark()
-{	
-	
-	
-	bgSprite1.loadGraphic(Paths.image("dialogue/backgrounds/spooky-room-dark"));
-
-	
-}
-
-function bgCreateSpookyRoom()
-{	
-	
-	
-	bgSprite1.loadGraphic(Paths.image("dialogue/backgrounds/spooky-room"));
-
+	bgSpriteTraceShit = sprite;
+	trace("Background sprite set to: " + '"' +bgSpriteTraceShit + '"');
 	
 }
 
 function postNext(event)
 {
+	handleVoices();
 	trace(curLine.char + " says: " + curLine.text);
-	if (curLine.char == "megablade")
+	if (voiceNumShit == 0)
 	{
 		bgEdit("evil-room");
-		trace("sprite made");
+		
 	}
-	if (curLine.text == "I don’t know about this place, Pico...")
-	{
-		bgEdit("spooky-room-dark");
-		trace("sprite made");
-	}
-	if (curLine.text == "")
-	{
-		bgEdit("spooky-room");
-		trace("sprite made");
-	}
+	
+}
+
+function handleVoices()
+{
+	voiceNumShit++;
+	if (curVoiceline != null) curVoiceline.stop();
+	
+	
+	curVoiceline = FlxG.sound.play(Paths.sound(dialogueFilePath+dioList[voiceNumShit]));
+	
 }
