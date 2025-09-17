0449017691e2bc3a8589917aecc3455849dfcf0f
Astrial <mlsgamer09@gmail.com>
2025-09-17 12:25:39 -0500

    dialogue revamp: thriller

--- songs/thriller/dialogue.hx
+++ songs/thriller/dialogue.hx
@@ -1,54 +1,110 @@
-function next(event)
-{
-	if (!event.playFirst) trace("-");
-}
+import haxe.xml.Access;
+
+// Var things
+var songThing = "high-spirits";
+
+var curVoiceline;
+
+var dialogueFilePath = "dio/sounds/" + songThing + "/";
+
+var voiceNumShit = -1;
+
+var bgSpriteTraceShit;
+
+// holy fuck that's a lot of dialogue
+
+var dioList:Array<String> = 
+[
+    "1",
+    "2",
+	"3", 
+	"4", 
+	"5", 
+	"6",
+	"7", 
+	"8", 
+	"9", 
+	"10", 
+	"11", 
+	"12", 
+	"13", 
+	"14",
+	"15",
+	"16",
+	"17",
+	"none",
+	"18",
+	"19",
+	"20",
+	"21",
+	"22",
+	"23",
+	"24",
+	"25",
+	"26",
+	"27",
+	"28",
+	"29",
+	"30",
+	"31",
+	"32",
+	"33",
+	"34",
+	"35",
+	"36",
+	"37",
+	"38",
+	"39"
+];
+
+//BG Sprite
 
 var bgSprite1:FunkinSprite = new FunkinSprite(0,0);
-
 insert(members.indexOf = 1, bgSprite1);
 add(bgSprite1);
+
+// Functions
 
 function bgEdit(sprite)
 {	
 	
 	bgSprite1.loadGraphic(Paths.image("dialogue/backgrounds/"+sprite));
-	
-}
-
-function bgCreateSpookyRoomDark()
-{	
-	
-	
-	bgSprite1.loadGraphic(Paths.image("dialogue/backgrounds/spooky-room-dark"));
-
-	
-}
-
-function bgCreateSpookyRoom()
-{	
-	
-	
-	bgSprite1.loadGraphic(Paths.image("dialogue/backgrounds/spooky-room"));
-
+	bgSpriteTraceShit = sprite;
+	trace("Background sprite set to: " + '"' +bgSpriteTraceShit + '"');
 	
 }
 
 function postNext(event)
 {
+	handleVoices();
 	trace(curLine.char + " says: " + curLine.text);
-	if (curLine.char == "megablade")
+	if (voiceNumShit == 0)
 	{
 		bgEdit("spooky-house");
-		trace("sprite made");
+		
 	}
-	if (curLine.text == "I don’t know about this place, Pico...")
+	if (voiceNumShit == 6)
 	{
 		bgEdit("spooky-room-dark");
-		trace("sprite made");
+		
 	}
-	if (curLine.text == "")
+	if (voiceNumShit == 18)
 	{
 		bgEdit("spooky-room");
-		trace("sprite made");
+		
 	}
+	
+}
+
+function handleVoices()
+{
+	voiceNumShit++;
+	if (curVoiceline != null) curVoiceline.stop();
+	
+	if (curVoiceLind != 18)
+	{
+		curVoiceline = FlxG.sound.play(Paths.sound(dialogueFilePath+dioList[voiceNumShit]));
+	}
+	
+	
 }
