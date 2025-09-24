a24bbc84d9a305f5bf2256f1015c7b1d9ed913cf
Astrial <mlsgamer09@gmail.com>
2025-09-24 14:40:49 -0500

    Create dialogue.hx

---
+++ songs/stars aligned/dialogue.hx
@@ -0,0 +1,101 @@
+import haxe.xml.Access;
+
+// Var things
+var songThing = "stars-aligned";
+
+var curVoiceline;
+
+var dialogueFilePath = "dio/sounds/" + songThing + "/";
+
+var voiceNumShit = -1;
+
+var bgSpriteTraceShit;
+
+var dioList:Array<String> = 
+[
+    "1",
+    "2",
+  	"3", 
+  	"4",
+  	"5",
+  	"6",
+  	"7", 
+  	"8", 
+  	"9", 
+  	"10", 
+  	"11", 
+    "none", 
+  	"12", 
+  	"13", 
+  	"14", 
+  	"15", 
+  	"16", 
+  	"17", 
+  	"18", 
+  	"19", 
+  	"20", 
+  	"21", 
+  	"22", 
+  	"23", 
+    "none",  
+  	"24', 
+  	"25", 
+  	"26", 
+  	"27", 
+  	"28", 
+  	"29", 
+  	"30", 
+  	"31", 
+  	"32", 
+  	"33", 
+  	"34"
+];
+
+//BG Sprite
+
+var bgSprite1:FunkinSprite = new FunkinSprite(0,0);
+insert(members.indexOf = 1, bgSprite1);
+add(bgSprite1);
+
+// Functions
+
+function bgEdit(sprite)
+{	
+	
+	bgSprite1.loadGraphic(Paths.image("dialogue/backgrounds/"+sprite));
+	bgSpriteTraceShit = sprite;
+	trace("Background sprite set to: " + '"' +bgSpriteTraceShit + '"');
+	
+}
+
+function update()
+{
+	bgSprite1.setGraphicSize(FlxG.width, FlxG.height);
+	bgSprite1.updateHitbox();
+}
+
+function postNext(event)
+{
+	handleVoices();
+	trace(curLine.char + " says: " + curLine.text);
+	if (voiceNumShit == 0)
+	{
+		bgEdit("bench");	
+}
+
+function handleVoices()
+{
+	voiceNumShit++;
+	if (curVoiceline != null) curVoiceline.stop();
+	if (voiceNumShit != "none" && voiceNumShit != "noneend" && voiceNumShit != "")
+	{
+		curVoiceline = FlxG.sound.play(Paths.sound(dialogueFilePath+dioList[voiceNumShit]));
+	}
+	
+	
+}
+
+function destroy()
+{
+	if (curVoiceline != null && !curVoiceline.persist) curVoiceline.destroy();
+}
